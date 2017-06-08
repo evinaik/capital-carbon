3eae6f419740c7987c26dad981a82cf61b1b16bc
Ekansh Vinaik <Ekansh.Vinaik@capitalone.com>
2017-06-07 22:20:42 -0400

    fix conflict

---
+++ client/app/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="w3-container w3-theme-teal w3-padding-16">
+    <h3>A MIGOS TEAM</h3>
+  </footer>
+);
+
+export default Footer;
