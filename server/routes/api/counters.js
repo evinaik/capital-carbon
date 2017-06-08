065ca6f5cc8213dbd642f28e45b35c80cd2064de
Ekansh Vinaik <ekansh@umd.edu>
2017-06-08 01:17:37 -0400

    move map reduce to node

--- server/routes/api/counters.js
+++ server/routes/api/counters.js
@@ -1,3 +1,4 @@
+var request = require('request');
 const Counter = require('../../models/Counter');
 
 module.exports = (app) => {
@@ -6,6 +7,25 @@ module.exports = (app) => {
       .exec()
       .then((counter) => res.json(counter))
       .catch((err) => next(err));
+  });
+
+  app.get('/api/purchases', (req, res, next) => {
+    request('http://api.reimaginebanking.com/accounts/59381913a73e4942cdafd797/purchases?key=d313680b12769feb817a469882c676a4', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var j = JSON.parse(body);
+        var n = j.reduce(function (pv, cv) {
+          if (pv[cv.merchant_id]) {
+            pv[cv.merchant_id] += cv.amount;
+          } else {
+            pv[cv.merchant_id] = cv.amount;
+          }
+
+          return pv;
+        }, {});
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(n));
+      }
+    })
   });
 
   app.post('/api/counters', function (req, res, next) {
