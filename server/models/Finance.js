a6c234ae668bb9e9b7594f8ee84c2157ab7e411a
Ekansh Vinaik <ekansh@umd.edu>
2017-06-08 09:39:10 -0400

    add api

---
+++ server/models/Finance.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const FinanceSchema = new mongoose.Schema({
+  company: String,
+  updated: { type: Date, default: Date.now },
+  volume: { type: Number, default: 0 },
+  description: String,
+  news: String,
+  open: { type: Number, default: 0 },
+  close: { type: Number, default: 0 },
+  high: { type: Number, default: 0 },
+  low: { type: Number, default: 0 },
+  sales: { type: Number, default: 0 },
+  hopen: { type: Number, default: 0 },
+  hclose: { type: Number, default: 0 },
+  hhigh: { type: Number, default: 0 },
+  hlow: { type: Number, default: 0 },
+  hsales: { type: Number, default: 0 },
+});
+
+module.exports = mongoose.model('Finance', FinanceSchema);
--- server/routes/api/counters.js
+++ server/routes/api/counters.js
@@ -1,5 +1,10 @@
 var request = require('request');
-const Counter = require('../../models/Counter');
+var googleFinance = require('google-finance');
+var _ = require('lodash');
+var util = require('util');
+const Finance = require('../../models/Finance');
+const mongoose = require('mongoose');
+
 
 module.exports = (app) => {
   app.get('/api/counters', (req, res, next) => {
@@ -9,7 +14,7 @@ module.exports = (app) => {
       .catch((err) => next(err));
   });
 
-  app.get('/api/purchases', (req, res, next) => {
+  app.get('/api/info/:year/:month/:day', (req, res, next) => {
     request('http://api.reimaginebanking.com/accounts/59381913a73e4942cdafd797/purchases?key=d313680b12769feb817a469882c676a4', function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var j = JSON.parse(body);
@@ -22,10 +27,70 @@ module.exports = (app) => {
 
           return pv;
         }, {});
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(n));
+
+        var symbols = [
+          'AAPL',
+          'GOOGL',
+          'AMZN',
+          'FB',
+        ]
+
+        mongoose.connection.collections['finances'].drop(function (err) {
+          // console.log('collection dropped');
+        });
+
+        var nsymbols = symbols.map(function (x) {
+          return 'NASDAQ:' + x;
+        });
+
+        var from = req.params.year + '-' + (req.params.month - 1) + '-' + req.params.day;
+        var to = req.params.year + '-' + req.params.month + '-' + req.params.day;
+
+        var ret = [];
+
+        googleFinance.historical({
+          symbols: nsymbols,
+          from: from,
+          to: to,
+        }, function (err, result) {
+          _.each(result, function (quotes, symbol) {
+            var temp = nsymbols.indexOf(symbol);
+            var kys = Object.keys(n);
+            var fin = new Finance({
+              company: symbols[temp],
+              volume: n[kys[temp]],
+              open: quotes[0] ? quotes[0].open : 0,
+              close: quotes[0] ? quotes[0].close : 0,
+              high: quotes[0] ? quotes[0].high : 0,
+              low: quotes[0] ? quotes[0].low : 0,
+              sales: quotes[0] ? quotes[0].volume : 0,
+              hopen: quotes[quotes.length - 1] ? quotes[quotes.length - 1].open : 0,
+              hclose: quotes[quotes.length - 1] ? quotes[quotes.length - 1].close : 0,
+              hhigh: quotes[quotes.length - 1] ? quotes[quotes.length - 1].high : 0,
+              hlow: quotes[quotes.length - 1] ? quotes[quotes.length - 1].low : 0,
+              hsales: quotes[quotes.length - 1] ? quotes[quotes.length - 1].volume : 0,
+            });
+
+            ret.push(fin);
+          })
+          googleFinance.companyNews({
+            symbols: nsymbols,
+          }, function (err, result) {
+            _.each(result, function (news, symbol) {
+              var temp = nsymbols.indexOf(symbol);
+              var kys = Object.keys(n);
+              ret[temp].news = news[0].title || '';
+              console.log(ret[temp]);
+              ret[temp].save()
+                .then()
+                .catch((err) => next(err));
+            })
+
+            res.end(JSON.stringify(ret));
+          })
+        });
       }
-    })
+    });
   });
 
   app.post('/api/counters', function (req, res, next) {
